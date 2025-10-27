8bd05b3a443a4f20c8f07bd11118618d4f8d5d80
DawidHr <dawiid21@gmail.com>
2025-10-27 19:28:31 +0100

    add PostMapping to crate account

--- src/main/java/com/dawidhr/BillingService/controller/Account.java
+++ src/main/java/com/dawidhr/BillingService/controller/Account.java
@@ -3,6 +3,7 @@ package com.dawidhr.BillingService.controller;
 import com.dawidhr.BillingService.dto.account.AccountDto;
 import com.dawidhr.BillingService.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class Account {
     @Autowired
     AccountService accountService;
 
+    @PostMapping
     public void create(@RequestBody AccountDto accountDto) {
         accountService.crate(accountDto);
     }
