fd7f9772247566d28379e21cc7e6fe0feffb14d6
DawidHr <dawiid21@gmail.com>
2026-02-01 21:10:21 +0100

    Add consumes and produce to BillController

--- src/main/java/com/dawidhr/BillingService/controller/BillController.java
+++ src/main/java/com/dawidhr/BillingService/controller/BillController.java
@@ -3,13 +3,14 @@ package com.dawidhr.BillingService.controller;
 import com.dawidhr.BillingService.dto.bill.BillDto;
 import com.dawidhr.BillingService.service.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/bill")
+@RequestMapping(value="/api/bill", consumes=MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class BillController {
 
     @Autowired
