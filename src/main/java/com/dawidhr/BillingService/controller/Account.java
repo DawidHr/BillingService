1a6a554620d4dc82588859345ee2242ab1fdc256
DawidHr <dawiid21@gmail.com>
2026-02-01 21:26:23 +0100

    AccountController add consumes and produces

--- src/main/java/com/dawidhr/BillingService/controller/Account.java
+++ src/main/java/com/dawidhr/BillingService/controller/Account.java
@@ -5,10 +5,11 @@ import com.dawidhr.BillingService.dto.account.AccountRemindPasswordRequest;
 import com.dawidhr.BillingService.dto.account.AccountUpdatePassword;
 import com.dawidhr.BillingService.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping(value = "/api/account", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class Account {
 
     @Autowired
