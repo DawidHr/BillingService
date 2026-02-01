4314c9e9039778660a5838e7b4dc752eeddcf6cb
DawidHr <dawiid21@gmail.com>
2026-02-01 21:25:45 +0100

    BillServiceMvcTest - clean imports

--- src/test/java/com/dawidhr/BillingService/service/BillServiceMvcTest.java
+++ src/test/java/com/dawidhr/BillingService/service/BillServiceMvcTest.java
@@ -1,18 +1,12 @@
 package com.dawidhr.BillingService.service;
 
-import com.dawidhr.BillingService.config.GsonConfig;
-import com.dawidhr.BillingService.controller.BillController;
-
 import com.dawidhr.BillingService.dto.bill.BillDto;
 import com.dawidhr.BillingService.dto.bill.BillItemDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
